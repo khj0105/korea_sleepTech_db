6734ed75094fe5f428b9f9b3c9af35505ac823cd
khj0105 <khj0001052@gmail.com>
2025-04-29 17:29:57 +0900

    20250429 DB UPLOAD

--- springboot_db.sql
+++ springboot_db.sql
@@ -18,3 +18,15 @@ CREATE TABLE student (
 );
 
 SELECT * FROM student;
+
+-- book 테이블 --
+CREATE TABLE book (
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    writer VARCHAR(50) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    content VARCHAR(500) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    CONSTRAINT chk_category CHECK (category IN ('NOVEL', 'ESSAY', 'POEM', 'MAGAZINE'))
+);
+
+SELECT * FROM book;
